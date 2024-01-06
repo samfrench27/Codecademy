90b6b701968d8d12383bfaaabe321a5c0cb71085
samfrench27 <150862015+samfrench27@users.noreply.github.com>
2024-01-06 00:59:24 +0000

    Added comments to improve readability.

--- mixedMessages.js
+++ mixedMessages.js
@@ -3,19 +3,23 @@ let starSigns= ['Aries', 'Taurus', 'Gemini','Cancer','Leo','Virgo','Libra','Scor
 let userPositiveMessage=['Good fortune awaits you','This will be a great year for you','Youre doing great!'];
 let userAdvice=['Be careful who you trust','Keep working hard', 'Persistance pays off'];
 
+//This code will generate a Random star sign.
 const generateRandomStarSign = () => {
     let num= Math.floor(Math.random()*starSigns.length);
     return starSigns[num];
 }
+//This code will generate a positive message from the userPositiveMessage array.
 const generatePositiveMessage = () => {
     let num= Math.floor(Math.random()*userPositiveMessage.length);
     return userPositiveMessage[num];
 }
+////This code will generate a piece of advice from the userAdvice array.
 const generateUserAdvice = () => {
     let num= Math.floor(Math.random()*userAdvice.length);
     return userAdvice[num];
 }
 
+//This code will format the concatinated output to console. 
 console.log("Star sign: " + generateRandomStarSign());
 console.log("Message: " + generatePositiveMessage());
 console.log("Advice: " + generateUserAdvice());
